ef9fc2784f7a82d799993bf204604b8b9097eda8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-21 22:20:32 +0300

    Создал слайдер. Строго не судите

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import './App.css';
+import './components/Slider';
+import React, { Component } from 'react';
+import './App.css';
+import Slider from './components/Slider';
+
+
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
 function App() {
   return (
     <div>
       <h1>Briskly</h1>
+      <div><Slider/></div>
     </div>
   );
 }
