20433ba7b382e010a3eb4914df699fcfc175fc6e
Vitaliy <vitalyazems@gmail.com>
2023-05-29 00:01:47 +0600

    Небольшие улучшения

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
+import './styles/index.scss';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
 import MainCarousel from '../components/MainCarousel';
-import CarouselReviews from '../components/carousel_reviews/CarouselReviews'
-
-
+import CarouselReviews from '../components/CarouselReviews';
 
 function Homepage() {
   return (
-  <>
-  <MainCarousel/>
-  
-  <CarouselReviews/>
-  </>
-  )
+    <>
+      <MainCarousel />
+
+      <CarouselReviews />
+    </>
+  );
 }
 
 export default Homepage;
