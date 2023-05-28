2563427332b12704adb5f931cf95cae6e36d31f7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-28 21:12:45 +0300

    DEV-21.1

--- src/pages/TestsPage.jsx
+++ src/pages/TestsPage.jsx
@@ -1,7 +1,10 @@
+import EnglishQuiz from "../components/english-quiz/EnglishQuiz";
+
 function TestsPage() {
   return (
     <div className="container-fluid text-center py-5">
       <h2>Тесты</h2>
+      <EnglishQuiz/>
         <div className="layer1"></div>
     </div>
   );
