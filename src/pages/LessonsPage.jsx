13ce3dce481a8dfd00659f8b51b5b2271ce2d523
Vitaliy <vitalyazems@gmail.com>
2023-05-28 13:03:09 +0600

    Добавил страницу урока

--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -11,20 +11,20 @@ function LessonsPage() {
   ];
   return (
     <div className="container-fluid text-center py-5">
-      <h2>Lessons page</h2>
+      <h2 className="mb-5">Lessons page</h2>
       <div className="d-flex justify-content-center">
         <div className="row row-cols-3">
           {lessons.map((lesson) => (
-            <div className="col">
-              <Card key={lesson.id} className="col mb-3">
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <div key={lesson.id} className="col mb-3">
+              <Card>
+                <Card.Img variant="top" src="img/lesson-placeholder.svg" />
                 <Card.Body>
                   <Card.Title>{lesson.title}</Card.Title>
                   <Card.Text>
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </Card.Text>
-                  <Link to={`/lessons/${lesson.id}`}>Go somewhere</Link>
+                  <Link to={`/lessons/${lesson.id}`}>Перейти</Link>
                 </Card.Body>
               </Card>
             </div>
--- src/pages/SingleLessonPage.jsx
+++ src/pages/SingleLessonPage.jsx
@@ -2,8 +2,13 @@ import { useParams } from 'react-router-dom';
 
 function SingleLessonPage() {
   const { id } = useParams();
-  console.log(id);
-  return <h2>lesson-{id}</h2>;
+
+  return (
+    <div className="container-fluid text-center py-5">
+      <h2 className="mb-5">Lesson-{id}</h2>
+      <img src="/img/video-player-placeholder.gif" alt="..." />
+    </div>
+  );
 }
 
 export default SingleLessonPage;
