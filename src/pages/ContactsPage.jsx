ea61aa53e2822362c89cf0ab8063f17c4b93ca9b
YasSpyder <yasspyder@gmail.com>
2023-05-29 18:49:43 +0300

    carousel and conacts edited

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -2,7 +2,14 @@ function ContactsPage() {
   return (
     <div className="container-fluid text-center py-5">
       <h2>Контакты</h2>
-        <div className="layer1"></div>
+        <div className="layer1">
+            <img src="img/map.jpg" alt="" />
+            <div className="layercontacts">
+            Вы можете связаться с нами по телефону: +7 (903) 134-56-77 или отправить мне сообщение по
+            e-mail:
+                <a href="mailto:1@brisklylearn.ru">1@brisklylearn.ru</a>
+            </div>
+        </div>
     </div>
   );
 }
