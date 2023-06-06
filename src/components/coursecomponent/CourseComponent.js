24a14791ee48432a4119da6cb33a822f68c183ad
Vitaliy <vitalyazems@gmail.com>
2023-06-06 17:48:53 +0600

    small improvements

--- src/components/coursecomponent/CourseComponent.js
+++ src/components/coursecomponent/CourseComponent.js
@@ -1,22 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { fetchCourses } from './../../store/slices/coursesSlice';
 import Loader from './../Loader';
-// import style from "./CourseComponent.module.css";
 
-
-
-function CourseComponent() {
+function CourseComponent({ courses }) {
   const loading = useSelector((state) => state.courses.loading);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCourses());
-  }, [dispatch]);
-
-  const courses = useSelector((state) => state.courses.courses.slice(0, 3));
 
   return (
     <div className="container-fluid text-center py-5">
@@ -24,16 +12,24 @@ function CourseComponent() {
         <Loader />
       ) : (
         <>
-          <h2 className="mb-5 ">Курсы</h2>
+          <h2 className="mb-5">Курсы</h2>
           <div className="d-flex justify-content-center">
-            <div className="row row-cols-3">
+            <div className="row row-cols-3 w-100">
               {courses.map((course) => (
                 <div key={course.id} className="col mb-3">
                   <Card>
-                    <Card.Img variant="top" src={course.image_url} />
+                    <Card.Img
+                      variant="top"
+                      src={course.image_url}
+                      style={{ objectFit: 'cover', height: '250px' }}
+                    />
                     <Card.Body>
                       <Card.Title>{course.name}</Card.Title>
-                      <Card.Text>{course.description}</Card.Text>
+                      <Card.Text
+                        style={{ height: '100px', overflow: 'scroll' }}
+                      >
+                        {course.description}
+                      </Card.Text>
                       <Link
                         to={course.id === 1 ? `/courses/${course.id}` : '#'}
                         style={
@@ -54,8 +50,6 @@ function CourseComponent() {
       )}
     </div>
   );
- 
-
 }
 
 export default CourseComponent;
