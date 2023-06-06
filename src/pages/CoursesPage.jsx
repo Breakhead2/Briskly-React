72620a55e7010e67616e61622b12f091708319d1
Vitaliy <vitalyazems@gmail.com>
2023-06-06 17:55:51 +0600

    bug fixes

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -1,7 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCourses } from '../store/slices/coursesSlice';
 import CourseComponent from '../components/coursecomponent/CourseComponent';
 
 function CoursesPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCourses());
+  }, [dispatch]);
+
   const courses = useSelector((state) => state.courses.courses);
 
   return <CourseComponent courses={courses} />;
