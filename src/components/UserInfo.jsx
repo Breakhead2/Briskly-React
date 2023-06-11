6853e2566c5954d7361a97cee80ed108a9745500
Vitaliy <vitalyazems@gmail.com>
2023-06-11 21:27:20 +0600

    add dropdown in UserInfo component

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
 function UserInfo() {
   const profile = useSelector((state) => state.profile.profile);
@@ -11,10 +12,34 @@ function UserInfo() {
         style={{ height: '100%' }}
       >
         {profile ? (
-          <div className="p-3 d-flex align-items-center border border-secondary rounded">
-            <img src={profile.image_url} alt="..." style={{ width: '30px' }} />
-            <h6 className="mb-0 ml-2">{profile.name}</h6>
-          </div>
+          <Dropdown>
+            <div className="p-3 d-flex align-items-center border border-secondary rounded">
+              <img
+                src={profile.image_url}
+                alt="..."
+                style={{ width: '30px' }}
+              />
+              <h6 className="mb-0 ml-2">{profile.name}</h6>
+              <span className="ml-3">
+                55
+                <i className="fa fa-bolt text-primary ml-1"></i>
+              </span>
+              <Dropdown.Toggle
+                variant="secondary"
+                id="dropdown-basic"
+                className="ml-2"
+              ></Dropdown.Toggle>
+            </div>
+
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Link to="/profile" className="text-decoration-none">
+                  Профиль
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item>Выйти</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         ) : (
           <Link
             className="btn py-2 px-4 ml-auto d-none d-lg-block"
