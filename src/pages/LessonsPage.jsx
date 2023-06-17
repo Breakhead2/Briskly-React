a4b183eb9267f7d07266f009005ad0b15f29eb3c
vitaliyzems <94466722+vitaliyzems@users.noreply.github.com>
2023-06-17 17:48:03 +0300

    decomposed the card component and added a progress label (#30)

--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -1,48 +1,40 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchallLessons } from '../store/slices/allLessonsSlice';
-import { Card } from 'react-bootstrap';
 import Loader from '../components/Loader';
+import CardComponent from '../components/CardComponent';
 
 function LessonsPage() {
   const { id } = useParams();
   const loading = useSelector((state) => state.allLessons.loading);
+  const lessons = useSelector((state) => state.allLessons.lessons);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchallLessons(id));
   }, [dispatch, id]);
 
-  const lessons = useSelector((state) => state.allLessons.lessons);
-
   return (
-    <div className="container-fluid text-center py-5">
+    <div className="container-fluid text-center">
       {loading ? (
         <Loader />
       ) : (
         <>
           <h2 className="mb-5">Уроки</h2>
           <div className="d-flex justify-content-center">
-            <div className="row row-cols-3 w-100">
-              <div className="col mb-3">
-                {lessons.map((lesson) => {
-                  return (
-                    <Link
-                      to={`/lessons/${lesson.id}`}
-                      key={lesson.id}
-                      className="text-decoration-none"
-                    >
-                      <Card>
-                        <Card.Img variant="top" src={lesson.image} />
-                        <Card.Body>
-                          <Card.Title>{lesson.heading}</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </Link>
-                  );
-                })}
-              </div>
+            <div className="row w-100">
+              {lessons.map(({ id, image, heading }) => {
+                return (
+                  <CardComponent
+                    key={id}
+                    img={image}
+                    title={heading}
+                    complete={true}
+                    linkPath={`/lessons/${id}`}
+                  />
+                );
+              })}
             </div>
           </div>
         </>
--- src/pages/TestsPage.jsx
+++ src/pages/TestsPage.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
-import { Container, CardColumns } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTests } from '../store/slices/allTestsSlice';
 import Loader from '../components/Loader';
+import CardComponent from '../components/CardComponent';
 
 function TestsPage() {
   const dispatch = useDispatch();
@@ -15,27 +14,29 @@ function TestsPage() {
   }, [tests, dispatch]);
 
   return (
-    <Container fluid className="text-center py-5">
+    <div className="container-fluid text-center">
       {loading ? (
         <Loader />
       ) : (
         <>
           <h2 className="mb-5">Тесты</h2>
-          <CardColumns className='d-sm-inline-flex justify-content-sm-around'>
-            {tests.map((test) => (
-              <Link key={test.id} to={`/tests/${test.id}`} className="card-test-link">
-                <div className="card card-test mb-3">
-                  <img src={test.image_url} className="card-img-top" alt={test.name} />
-                  <div className="card-body">
-                    <h5 className="card-title">{test.name}</h5>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </CardColumns>
+          <div className="d-flex justify-content-center">
+            <div className="row w-100">
+              {tests.map(({ id, image_url, name }) => {
+                return (
+                  <CardComponent
+                    key={id}
+                    img={image_url}
+                    title={name}
+                    linkPath={`/tests/${id}`}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </>
       )}
-    </Container>
+    </div>
   );
 }
 
