ab46066fd1470f947ea5fc39825354b9275f4b36
Denis Sazonov <81635087+Breakhead2@users.noreply.github.com>
2023-06-21 17:12:59 +0400

    import getCookie func on allLessonsSlice

--- src/store/slices/allLessonsSlice.js
+++ src/store/slices/allLessonsSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import getCookie from '../../services/getCookie';
 
 export const fetchallLessons = createAsyncThunk(
   'lesson/fetchLessonsByCourseId',
