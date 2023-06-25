01d6a19fd44cf9ff94883ec9b62f43b1e82bde77
Denis Sazonov <greenfish@list.ru>
2023-06-25 12:19:20 +0400

    start develop articles

--- src/pages/ArticlesPage.jsx
+++ src/pages/ArticlesPage.jsx
@@ -2,7 +2,6 @@ function ArticlesPage() {
   return (
     <div className="container-fluid text-center py-5">
       <h2>Статьи</h2>
-        <div className="layer1"><h4> Содержание</h4></div>
     </div>
   );
 }
