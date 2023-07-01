44eb5405b3b773bf532ba756db4ed84266c8e5d5
Denis Sazonov <greenfish@list.ru>
2023-07-01 10:51:03 +0400

    start review send

--- src/pages/AddReviewPage.jsx
+++ src/pages/AddReviewPage.jsx
@@ -1,13 +1,21 @@
-import { Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 function AddReviewPage() {
+  const [review, setReview] = useState("");
   return (
     <div className="container-fluid py-5">
       <h2 className="text-center">Оставить отзыв</h2>
       <Form style={{ width: 500 }}>
         <Form.Group>
           <Form.Label>Напишите своё мнение о нас</Form.Label>
-          <Form.Control as="textarea" type="text" rows={5} />
+          <Form.Control
+            as="textarea"
+            type="text"
+            rows={5}
+            value={review}
+            onChange={(e) => setReview(e.target.value)}
+          />
         </Form.Group>
         <Button disabled>Отправить</Button>
       </Form>
