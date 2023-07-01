869831473fb4a07018eec35b3e22dcce5055034b
Denis Sazonov <greenfish@list.ru>
2023-07-01 11:25:06 +0400

    create send review

--- src/pages/AddReviewPage.jsx
+++ src/pages/AddReviewPage.jsx
@@ -1,14 +1,59 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchSendReview } from "../store/slices/reviewSlice";
+import { showModal } from "../store/slices/modalSlice";
 import { Button, Form } from "react-bootstrap";
 
 function AddReviewPage() {
   const [review, setReview] = useState("");
+  const dispatch = useDispatch();
+  const simpleValidation = () => {
+    if (!review.length) {
+      dispatch(
+        showModal({
+          message: "Необходимо заполнить форму",
+        })
+      );
+      return false;
+    } else if (review.length < 5) {
+      dispatch(
+        showModal({
+          message: "Текст должен быть не менее 5 символов",
+        })
+      );
+      return false;
+    } else if (review.length > 455) {
+      dispatch(
+        showModal({
+          message: "Слишком длинный текст.",
+        })
+      );
+      return false;
+    }
+    return true;
+  };
+  const handlerSendReview = (e) => {
+    e.preventDefault();
+    if (simpleValidation()) {
+      const data = { text: review };
+      dispatch(fetchSendReview(data));
+      dispatch(
+        showModal({
+          message: "Спасибо за ваш отзыв!",
+        })
+      );
+      setReview("");
+    }
+  };
   return (
     <div className="container-fluid py-5">
       <h2 className="text-center">Оставить отзыв</h2>
       <Form style={{ width: 500 }}>
         <Form.Group>
-          <Form.Label>Напишите своё мнение о нас</Form.Label>
+          <Form.Label>
+            Поделитесь вашим мнение о нас. Нам очень важна обратная связь, чтобы
+            стать лучшей платформой обучения для вас
+          </Form.Label>
           <Form.Control
             as="textarea"
             type="text"
@@ -17,7 +62,9 @@ function AddReviewPage() {
             onChange={(e) => setReview(e.target.value)}
           />
         </Form.Group>
-        <Button disabled>Отправить</Button>
+        <Button type="submit" onClick={handlerSendReview}>
+          Отправить
+        </Button>
       </Form>
     </div>
   );
